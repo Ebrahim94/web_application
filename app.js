38d635d9bb5dd10421977d0096320ee2d5f4e0bd
Anonymous <home@Anonymouss-MacBook-Pro.local>
2019-10-30 17:04:57 -0400

    second draft

---
+++ app.js
@@ -0,0 +1,26 @@
+// SERVER------------------------------------------------------------------
+// importing dependancies
+const express = require("express")
+const bodyParser = require("body-parser")
+
+// Initializing express application
+let app = express();
+
+// instructing the express app
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// instructing the server on what to do when root route is requested
+app.get('/', function(req,res){
+  res.render('home');
+})
+
+app.get('/about',function(req,res){
+  res.render('about')
+})
+
+//Running the server
+app.listen(3000, function(){
+  console.log("You are connected to the server");
+})
---
+++ public/index.js
@@ -0,0 +1,10 @@
+$('nav').removeClass('navbar-dark')
+$('nav').addClass('navbar-light')
+
+$(document).ready(function(){
+
+$('.icon').show( "scale", {percent: 200, direction: 'vertical' }, 2000 );
+  //
+  // $('.white-bend').animate({marginLeft: "+=4rem"});
+  // $('.white-bend').animate({marginTop: "+=1rem"});
+});
